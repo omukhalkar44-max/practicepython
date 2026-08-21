5c6515e3886eb9eb38d0be30a6f83ec7d4c390bd
Om Ukhalkar <omukhalkar44@gmail.com>
2026-08-21 18:08:35 +0530

    perform basic operation

--- panda_basics.py
+++ panda_basics.py
@@ -15,3 +15,27 @@ data = {'Name': ['om ',
         'Age': [25, 30, 35, 40],
         'City': ['New York', 'Los Angeles', 'Chicago', 'amt']}
 print(data)
+
+df = pd.DataFrame(data)
+total = df['Age'].sum()
+
+print(df)
+print("total age",total)
+
+df = pd.DataFrame(data)
+find_max_age = df['Age'].max()
+print("maxxx age",find_max_age)
+
+df= pd.DataFrame(data)
+value_counts = df['Age'].value_counts()
+print("value counts",value_counts)
+
+df= pd.DataFrame(data)
+upper_case_names = df['Name'].str.upper()
+
+print("upper case name",upper_case_names)
+
+df= pd.DataFrame(data)
+replaced_names = df['Name'].str.replace('om', 'kajukatliiiiiiiiiiiiiiiiiiii')
+print("replaced names", replaced_names)
+
