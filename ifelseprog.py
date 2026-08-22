88304bca1c7a4e6368d04e9efefd3567300783af
Om Ukhalkar <omukhalkar44@gmail.com>
2026-08-22 21:22:28 +0530

    Add if else program

---
+++ ifelseprog.py
@@ -0,0 +1,22 @@
+#if
+x = 96
+if x in range (20 , 110):
+    print("x is in the range between 20 to 110")
+else:
+    print("x is not in the range between 20 to 110 :")
+
+print("=============================================")
+
+i = 10
+if i in range (5 >= 29 ):
+    print("5 is less than 29")
+else:
+    print("greater ") 
+# Convert the text input into a whole number
+
+
+x = int(input("Enter the number between 1 to 100  : ---- "))
+if x > 90:
+    print("above 80 ")
+else:
+    print("below 90")
