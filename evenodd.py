d1ca3966f89297ed9f543d7f2174fdc2764a9df8
Om Ukhalkar <omukhalkar44@gmail.com>
2026-08-23 14:46:09 +0530

    even odd , factorial

---
+++ evenodd.py
@@ -0,0 +1,19 @@
+#check even or odd number
+
+num = int(input("enter a number: "))
+if num % 2 ==0:
+    print(f"{num} even number")
+elif num % 2 != 0:
+    print(f"{num} odd number")
+
+
+#factorial
+
+
+X = int(input("Enter first factorial number: "))
+
+for i in range(1,X):
+
+    X = X * i
+
+print(X)
