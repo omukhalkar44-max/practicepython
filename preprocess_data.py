46114039370731763c77e414b7dd9692fe316a3e
Om Ukhalkar <omukhalkar44@gmail.com>
2026-08-25 17:16:52 +0530

    clean data using pandas and numpy

---
+++ preprocess_data.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+data = {'Name': ['Ashu', 'yash', 'jay', 'vedant'],
+        'Age': [25, 30, 35, 40],
+        'City': ['akola', 'amravati', 'nagpur', 'pune']}  
+dataframe = pd.DataFrame(data)
+show_dataframe = dataframe.head()
+pd.DataFrame = show_dataframe
+print("DataFrame:")
+print(dataframe)
